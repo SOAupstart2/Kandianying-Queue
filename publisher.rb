57495503e73c7004f8b031c594eeac801912375b
stozuka <stozuka@gmail.com>
2015-12-19 17:48:32 +0800

    publisher done

--- publisher.rb
+++ publisher.rb
@@ -4,27 +4,27 @@ require 'yaml'
 # Load keys
 keys = YAML.load(File.read('config/aws_key.yml'))
 
-# puts keys['production']['AWS_ACCESS_KEY_ID']
-# puts keys['production']['AWS_SECRET_ACCESS_KEY']
-# puts keys['production']['AWS_REGION']
-# puts keys['production']['AWS_URL']
-
 #Set credentials
-credentials = Aws::Credentials.new(keys['production']['AWS_ACCESS_KEY_ID'], keys['production']['AWS_SECRET_ACCESS_KEY'])
+credentials = Aws::Credentials.new(keys['sqs']['AWS_ACCESS_KEY_ID'], keys['sqs']['AWS_SECRET_ACCESS_KEY'])
 
 #Initialization
 sqs = Aws::SQS::Client.new(
-  region: keys['production']['AWS_REGION'],
+  region: keys['sqs']['AWS_REGION'],
   credentials: credentials,
 )
 
+# Set url and message
+queue_msg = 'test kandianying'
+queue_url = keys['sqs']['AWS_URL']
+
 #Send message
 sqs.send_message({
-  queue_url: keys['production']['AWS_URL'],
-  message_body: 'test kandianying',
+  queue_url: queue_url,
+  message_body: queue_msg,
   message_attributes: {
     "String" => {
-      data_type: "String"
+      string_value: "String",
+      data_type: "String",
     },
   },
 })
